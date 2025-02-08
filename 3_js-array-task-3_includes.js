7600d97626e6822097a1c094d2995fb29d031387
ronnie012 <ronnie.247365.1@gmail.com>
2025-02-09 01:02:03 +0600

    Successfully executed all the instructions of task-3

---
+++ 3_js-array-task-3_includes.js
@@ -0,0 +1,12 @@
+let codingBooks = ['Growing through struggle', 'JavaScript Er Oligoli', 'Programming Er 14 Goshthi'];
+// created an array of books containing different books.
+
+console.log(codingBooks.includes('JavaScript Er Oligoli'));
+// used includes method to check if the array contains a JavaScript book.
+
+if (codingBooks.includes('JavaScript Er Oligoli')) {
+console.log('Oh, finally gotcha!');
+} else {
+  console.log('Go find that immediately!');
+}
+//printed a message to the console indicating whether the element is present in the array or not.
