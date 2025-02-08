7a6fa7e1ee6cea826af72c94e01b3872986eebad
ronnie012 <ronnie.247365.1@gmail.com>
2025-02-09 00:29:51 +0600

    Practised the 2nd JavaScript Array task in details

---
+++ 2_js-array-task-2_add_remove.js
@@ -0,0 +1,11 @@
+let touristDestinations = ['cox-bazar', 'jaflong', 'sylhet-tea-garden'];//1. 3-indexed array declaration.
+
+touristDestinations.push('chittagong-hill-tracks');//2. added a new tourist destination to my array. 
+
+touristDestinations.push('shundarban', 'kuakata');//3. added two more to my array.
+
+touristDestinations.pop();//4. removed the last destination I've added.
+
+console.log(touristDestinations);//5. displayed the final array as output in the terminal.
+
+// console.log(touristDestinations.slice(0, -5));
