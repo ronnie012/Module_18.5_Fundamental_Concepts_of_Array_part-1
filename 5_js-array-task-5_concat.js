5f474f8fc8af93b6927e2815aa00ce397fae024f
ronnie012 <ronnie.247365.1@gmail.com>
2025-02-09 02:08:20 +0600

    Coded JavaScript Array task-5's solution successfully.

---
+++ 5_js-array-task-5_concat.js
@@ -0,0 +1,9 @@
+let array1 = [1, 3];
+let array2 = [5, 7];
+//1. created two arrays of my choice.
+
+console.log("Original array1: ", array1);
+console.log("Original array2: ", array2);
+console.log("Combined new array: ", array1.concat(array2));
+
+//2 and 3. used the concat method to combine the two arrays into a new array and printed both the original arrays and the combined array using console.log().
